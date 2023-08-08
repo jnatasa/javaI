f03711a1af7b41064dde69016796213ab1ba98e7
jnatasa <141272409+jnatasa@users.noreply.github.com>
2023-08-08 23:36:52 +0200

    Add files via upload

---
+++ part.4/Agent.java
@@ -0,0 +1,29 @@
+
+
+public class Agent {
+
+    private String firstName;
+    private String lastName;
+    
+    public Agent(String initFirstName, String initLastName) {
+        this.firstName = initFirstName;
+        this.lastName = initLastName;
+    }
+    
+    public String toString(){
+        return "My name is " + lastName + ", " + firstName + " " + lastName;   
+    }
+    
+public static void main(String[] args) {
+        
+        Agent bond = new Agent("James", "Bond");
+
+        bond.toString(); // prints nothing
+        System.out.println(bond);
+
+        Agent ionic = new Agent("Ionic", "Bond");
+        System.out.println(ionic);
+    }
+   } 
+    
+
---
+++ part.4/MainProgramm.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        
+        Statistics statistics = new Statistics();
+    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers:");
+       
+        
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+
+        
+        while (true){
+            int number = scanner.nextInt();
+            if(number == -1){
+                break;
+            }
+            
+            allNumbers.addNumber(number);
+            
+            if (number % 2 == 0){
+                evenNumbers.addNumber(number);
+            }else{
+                oddNumbers.addNumber(number);
+            }
+        }
+        
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+
+}
+}
+
