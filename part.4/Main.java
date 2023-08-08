342ba18df1c700e07495110a37f357bda54cc67a
jnatasa <141272409+jnatasa@users.noreply.github.com>
2023-08-08 12:46:45 +0200

    Add files via upload

--- part.4/Main.java
+++ part.4/Main.java
@@ -1,21 +1,8 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-        Debt mortgage = new Debt(120000.0, 1.20);
-        mortgage.printBalance();
-
-        mortgage.waitOneYear();
-        mortgage.printBalance();
-
-        int years = 0;
-
-        while (years < 20) {
-            mortgage.waitOneYear();
-            years = years + 1;
-        }
-
-        mortgage.printBalance();
-    }
-    
+  public static void main(String[] args) {
+    Song garden = new Song("In The Garden", 10910);
+    System.out.println("The song " + garden.name() + " has a length of " + garden.length() + " seconds.");
+  }
 }
